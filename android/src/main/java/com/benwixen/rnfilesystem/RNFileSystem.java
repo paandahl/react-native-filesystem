f47695b4e9c15da36b8524a6e2b13631ca3512c1
Preben Ludviksen <prebenl@gmail.com>
2016-10-29 13:42:03 +0400

    Added native code for Android.

---
+++ android/src/main/java/com/benwixen/rnfilesystem/RNFileSystem.java
@@ -0,0 +1,156 @@
+package com.benwixen.rnfilesystem;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class RNFileSystem extends ReactContextBaseJavaModule {
+
+  public RNFileSystem(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  public enum Storage {
+    BACKED_UP,
+    IMPORTANT,
+    AUXILIARY,
+    TEMPORARY
+  }
+
+  @Override
+  public String getName() {
+    return "RNFileSystem";
+  }
+
+  private String baseDirForStorage(Storage storage) {
+    switch (storage) {
+      case BACKED_UP:
+        return getReactApplicationContext().getFilesDir().getAbsolutePath() + "/BackedUp";
+      case IMPORTANT:
+        return getReactApplicationContext().getFilesDir().getAbsolutePath() + "/Important";
+      case AUXILIARY:
+        return getReactApplicationContext().getFilesDir().getAbsolutePath() + "/Auxiliary";
+      case TEMPORARY:
+        return getReactApplicationContext().getCacheDir().getAbsolutePath();
+      default:
+        throw new RuntimeException("Unrecognized storage: " + storage.toString());
+    }
+  }
+
+  private void createDirectories(File destination) {
+    int lastSlash = destination.getAbsolutePath().lastIndexOf("/");
+    String holdingFolderPath = destination.getAbsolutePath().substring(0, lastSlash);
+    File holdingFolder = new File(holdingFolderPath);
+    // noinspection ResultOfMethodCallIgnored
+    holdingFolder.mkdirs();
+  }
+
+  public void writeToFile(String relativePath, String content, Storage storage) throws IOException {
+    String baseDir = baseDirForStorage(storage);
+    File destination = new File(baseDir + "/" + relativePath);
+    createDirectories(destination);
+
+    OutputStreamWriter output = null;
+    try {
+      output = new OutputStreamWriter(new FileOutputStream(destination));
+      output.write(content);
+    } finally {
+      try {
+        if (output != null) {
+          output.close();
+        }
+      } catch (IOException ignored) {}
+    }
+  }
+
+  public String readFile(String relativePath, Storage storage) throws FileNotFoundException {
+    String baseDir = baseDirForStorage(storage);
+    File location = new File(baseDir + "/" + relativePath);
+
+    if (!location.exists()) {
+      throw new FileNotFoundException();
+    }
+
+    return new Scanner(location).useDelimiter("\\Z").next();
+  }
+
+  public boolean fileExists(String relativePath, Storage storage) {
+    String baseDir = baseDirForStorage(storage);
+    File location = new File(baseDir + "/" + relativePath);
+    return location.isFile();
+  }
+
+  public boolean directoryExists(String relativePath, Storage storage) {
+    String baseDir = baseDirForStorage(storage);
+    File location = new File(baseDir + "/" + relativePath);
+    return location.isDirectory();
+  }
+
+  private boolean deleteRecursive(File path) {
+    boolean ret = true;
+    if (path.isDirectory()){
+      for (File f : path.listFiles()){
+        ret = ret && deleteRecursive(f);
+      }
+    }
+    return ret && path.delete();
+  }
+
+  public boolean deleteFileOrDirectory(String relativePath, Storage storage) {
+    String baseDir = baseDirForStorage(storage);
+    File location = new File(baseDir + "/" + relativePath);
+    if (!location.exists()) {
+      return false;
+    }
+    deleteRecursive(location);
+    return true;
+  }
+
+  @ReactMethod
+  public void writeToFile(String relativePath, String content, String storage, Promise promise) {
+    try {
+      writeToFile(relativePath, content, Storage.valueOf(storage));
+      promise.resolve(true);
+    } catch (IOException e) {
+      promise.reject("ERROR", e.getMessage());
+    }
+  }
+
+  @ReactMethod
+  public void readFile(String relativePath, String storage, Promise promise) {
+    try {
+      String content = readFile(relativePath, Storage.valueOf(storage));
+      promise.resolve(content);
+    } catch (FileNotFoundException e) {
+      promise.reject("ERROR", "File was not found: " + relativePath);
+    }
+  }
+
+  @ReactMethod
+  public void fileExists(String relativePath, String storage, Promise promise) {
+    promise.resolve(fileExists(relativePath, Storage.valueOf(storage)));
+  }
+
+  @ReactMethod
+  public void directoryExists(String relativePath, String storage, Promise promise) {
+    promise.resolve(directoryExists(relativePath, Storage.valueOf(storage)));
+  }
+
+  @ReactMethod
+  public void delete(String relativePath, String storage, Promise promise) {
+    boolean deleted = deleteFileOrDirectory(relativePath, Storage.valueOf(storage));
+    if (!deleted) {
+      promise.reject("ERROR", "Could not delete item at path: " + relativePath);
+    } else {
+      promise.resolve(true);
+    }
+  }
+}
